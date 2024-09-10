1e980c2aa583f5f30f9410e66455aff1604e5aca
ptyants <to3nhom1lp93@gmail.com>
2024-09-10 23:58:43 +0700

    new update config

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require('express');
 const passport = require('passport');
 const methodOverride = require('method-override');
 const session = require('express-session');
-// const flash = require('connect-flash');
 const flash = require('express-flash');
 const mongoose = require('mongoose');
+const path = require('path');
+
 const app = express();
 
 // Cấu hình view engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Cấu hình thư mục chứa các tệp static (CSS, JS, ảnh, ...)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware để xử lý dữ liệu từ body request
 app.use(express.json()); // Middleware để xử lý JSON
@@ -26,11 +31,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-
 // Routes
-// const indexRoutes = require('./routes/'); // Import file index.js trong thư mục routes
 app.use('/', require('./routes')); // Sử dụng các route trong index.js
-
 
 // Kết nối tới MongoDB
 mongoose.connect('mongodb+srv://phamtheants:MjJg26IjuyUft9pu@webcscdb.tlk67.mongodb.net/CSC_db?retryWrites=true&w=majority', 
@@ -48,12 +50,8 @@ db.once('open', () => {
     console.log('Kết nối MongoDB thành công');
 
     // Khởi động server chỉ khi MongoDB đã kết nối
-    app.listen(3000, '127.0.0.1', () => {
-        console.log('Server chạy tại http://127.0.0.1:3000/');
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, '127.0.0.1', () => {
+        console.log(`Server chạy tại http://127.0.0.1:${PORT}/`);
     });
-
-    // Bắt đầu server
-    // const PORT = process.env.PORT || 3000;
-    // app.listen(PORT, () => {
-    //   console.log(`Server running on port ${PORT}`);
 });
